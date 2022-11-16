f3c6537caecff0b752b434161ef719fc3da6e4e6
Niklas Solomon <niklassolomon@gmail.com>
2022-11-15 17:53:45 -0800

    handle click function

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Weather-Dashboard
+
+## Goal:
+The goal of this assignment will be to build a webpage that displays weather data based on a city search from the user. The page should display the current weather in that city, as well as a five day forecast. Weather data should include the city name, the date, weather conditions, temperature, humidity, wind speed, and UV index. The UV index will be color coded to indicate normal, moderate, and severe levels. The city search will be saved and displayed on the page where the user can click on the city and retrieve city weather data.
+
+## How:
+
+## Why:
+
+## Result:
--- script.js
+++ script.js
@@ -1,55 +1,126 @@
 // Declare global variables
-var searchHistory = [];
-var weatherApiUrl = 'https://api.openweathermap.org';
 var apiKey = "cad0812010082bedc3a2cdcb7db795c2";
 
 // Declare DOM elements
+var searchHistory = [];
 var searchEl = document.querySelector('#search');
 var inputEl = document.querySelector('#input');
 var searchHistoryEl = document.querySelector('#history');
 var todayEl = document.querySelector('#today');
 var forecastEl = document.querySelector('#forecast');
 
-// Submit the form to fetch weather information
-    
-    // Fetch the city name from the text <input>
+dayjs.extend(window.dayjs_plugin_utc);
+dayjs.extend(window.dayjs_plugin_timezone);
 
+// Submit the form to fetch weather information
+function renderSearchHistory() {
+    searchHistoryEl.innerHTML = '';
+
+    for (var i = searchHistory.length - 1; i >= 0; i--) {
+        var btn = document.createElement('button');
+        btn.setAttribute('type', 'button');
+        btn.setAttribute('aria-controls', 'today forecast');
+        btn.classList.add('history-btn', 'btn-history');
+
+        btn.setAttribute('data-search', searchHistory[i]);
+        btn.textContent = searchHistory[i];
+        searchHistoryEl.append(btn);
+    }
+};    
+    // Fetch the city name from the text <input>
+function appendToHistory(search) {
+    if (searchHistory.indexOf(search) !== -1) {
+        return;
+    }
+    searchHistory.push(search);
+    localStorage.setItem('search-history', JSON.stringify(searchHistory));
+    renderSearchHistory();
+};
     // Call the 'fetchGeolocation' and pass the city name
 
 // Handle button clicks to fetch weather info
-
+function initSearchHistory() {
+    var storedHistory = localStorage.getItem('search-history');
+    if (storedHistory) {
+        searchHistory = JSON.parse(storedHistory);
+    }
+    renderSearchHistory();
+};
     // Get the city name from the clicked button's (event.target) data-city attribute
-
     // Call the 'fetchGeolocation' and pass the city name
 
 // FETCH geolocation data (Geocoding API)
-function fetchGeolocation( cityName ) {
+function fetchGeolocation(cityName) {
+    var requestUrl = `http://api.openweathermap.org/geo/2.5/direct?q=${cityName}&appid=cad0812010082bedc3a2cdcb7db795c2`;
 
-    var request = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=cad0812010082bedc3a2cdcb7db795c2`;
-
-    fetch(request)
+    fetch(requestUrl)
         .then(function(response) {
             return response.json();
         })
         .then(function(data) {
             console.log(data);
-
             // Access lat and lon from data
-
-            // Call fetchOneCall and pass through the lat and lon
+            if (!data[0]) {
+                alert('Input not recognized');
+            } else {
+                saveSearch(cityName);
+                // Call fetchOneCall and pass through the lat and lon
+                fetchOneCall(data[0]);
+            }
         })
-}
-// FETCH weather data (Onecall API)
-function fetchOneCall(lat, lon) {
-
-    var request = `https://api.openweathermap.org/data/2.5/onecall?appid=cad0812010082bedc3a2cdcb7db795c2&lat=${lat}&lon=${lon}&exclude=minutely,hourly&units=imperial`;
-
-    fetch(request)
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(data) {
-            console.log(data);
+        
+        .catch(function (err) {
+            console.error(err);
         });
 }
-fetchOneCall();
+// FETCH weather data (Onecall API)
+function fetchOneCall(location) {
+    var { lat } = location;
+    var { lon } = location;
+    var city = location.name;
+    var requestUrl = `https://api.openweathermap.org/data/2.5/onecall?appid=cad0812010082bedc3a2cdcb7db795c2&lat=${lat}&lon=${lon}&exclude=minutely,hourly&units=imperial`;
+
+    fetch(requestUrl)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+            displayObjects(city, data);
+        });
+}
+
+// Save the search history in local storage and render on page
+// function saveSearch(cityName) {
+    
+// }
+
+// Display the fetched objects
+// function displayObjects(city, data) {
+
+// }
+
+// Pass cityName to Geolocation on submit
+function handleSearch(error) {
+    if (!inputEl.value) {
+        return;
+    }
+
+    error.preventDefault();
+    var cityName = inputEl.value.trim();
+    fetchGeolocation(cityName);
+    inputEl.value = '';
+}
+
+function handleClick(e) {
+    if (!e.target.matches('.btn-history')) {
+        return;
+    }
+    var btn = e.target;
+    var search = btn.getAttribute('data-search');
+    fetchGeolocation(search);
+}
+
+// fetchOneCall();
+initSearchHistory();
+searchForm.addEventListener('submit',)
